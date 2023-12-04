641e89c11b41b696fca157a8e999d6f1b71c3570
Tremain <g00367973@atu.ie>
2023-12-04 09:38:30 +0000

    added person class for holding data (updated version)

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,10 @@
 package ie.atu;
 
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person myPerson = new Person("paul", "adress", "0861234567");
+        System.out.println(myPerson.toString());
     }
 }
