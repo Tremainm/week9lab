bc275487fa2ada68f89ef6b7d600a8fda04e5c6a
Tremain <g00367973@atu.ie>
2023-12-04 10:10:17 +0000

    added customer class inherited from person class

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -6,5 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Person myPerson = new Person("paul", "adress", "0861234567");
         System.out.println(myPerson.toString());
+
+        Customer myCustomer = new Customer("paul", "124", "1234", "91475", true);
+        System.out.println(myCustomer.toString());
+
     }
 }
